8aeb74cc914fbabde7d58a8f50cfcf61d252f347
MouadJebbari <jebbari.mouad.solicode@gmail.com>
2023-01-09 16:59:46 +0000

    v2

--- script.js
+++ script.js
@@ -1,7 +1,4 @@
-// its make a favourites meal array if its not exist in local storage
-if (localStorage.getItem("favouritesList") == null) {
-    localStorage.setItem("favouritesList", JSON.stringify([]));
-}
+
 
 // its fetch meals from api and return it
 async function fetchMealsFromApi(url,value) {
@@ -15,7 +12,6 @@ async function fetchMealsFromApi(url,value) {
 // its show's all meals card in main acording to search input value
 function showMealList(){
     let inputValue = document.getElementById("my-search").value;
-    let arr=JSON.parse(localStorage.getItem("favouritesList"));
     let url="https://www.themealdb.com/api/json/v1/1/search.php?s=";
     let html = "";
     let meals=fetchMealsFromApi(url,inputValue);
@@ -23,11 +19,6 @@ function showMealList(){
         if (data.meals) {
             data.meals.forEach((element) => {
                 let isFav=false;
-                for (let index = 0; index < arr.length; index++) {
-                    if(arr[index]==element.idMeal){
-                        isFav=true;
-                    }
-                }
                 if (isFav) {
                     html += `
                 <div id="card" class="card mb-3" style="width: 20rem;">
@@ -35,8 +26,8 @@ function showMealList(){
                     <div class="card-body">
                         <h5 class="card-title">${element.strMeal}</h5>
                         <div class="d-flex justify-content-between mt-5">
-                            <button type="button" class="btn btn-outline-light" onclick="showMealDetails(${element.idMeal})">More Details</button>
-                            <button id="main${element.idMeal}" class="btn btn-outline-light active" onclick="addRemoveToFavList(${element.idMeal})" style="border-radius:50%"><i class="fa-solid fa-heart"></i></button>
+                            <button type="button" class="btn btn-outline-light" onclick="showMealDetails(${element.idMeal})">Details</button>
+                            
                         </div>
                     </div>
                 </div>
@@ -48,8 +39,8 @@ function showMealList(){
                     <div class="card-body">
                         <h5 class="card-title">${element.strMeal}</h5>
                         <div class="d-flex justify-content-between mt-5">
-                            <button type="button" class="btn btn-outline-light" onclick="showMealDetails(${element.idMeal})">More Details</button>
-                            <button id="main${element.idMeal}" class="btn btn-outline-light" onclick="addRemoveToFavList(${element.idMeal})" style="border-radius:50%"><i class="fa-solid fa-heart"></i></button>
+                            <button type="button" class="btn btn-outline-light" onclick="showMealDetails(${element.idMeal})">Details</button>
+                            
                         </div>
                     </div>
                 </div>
@@ -62,9 +53,8 @@ function showMealList(){
                 <div class="container">
                     <div class="row justify-content-center">
                         <div class="col-md-12 text-center">
-                            <span class="display-1 d-block">404</span>
-                            <div class="mb-4 lead">
-                                The meal you are looking for was not found.
+                            <div class="mb-4 lead" style=">
+                            Le repas que vous recherchez n'a pas été trouvé.
                             </div>
                         </div>
                     </div>
@@ -82,9 +72,11 @@ function showMealList(){
     async function showMealDetails(id) {
         let url="https://www.themealdb.com/api/json/v1/1/lookup.php?i=";
         let html="";
+
         await fetchMealsFromApi(url,id).then(data=>{
             html += `
-            <div id="meal-details" class="mb-5">
+            
+            <button type="button "  onclick="clospopu()" id='btn_close' class="btn-close"></button>
                 <div id="meal-header" class="d-flex justify-content-around flex-wrap">
                 <div id="meal-thumbail">
                     <img class="mb-2" src="${data.meals[0].strMealThumb}" alt="" srcset="">
@@ -102,8 +94,55 @@ function showMealList(){
                 <div class="text-center">
                 <a href="${data.meals[0].strYoutube}" target="_blank" class="btn btn-outline-light mt-3">Watch Video</a>
                 </div>
-            </div>
+            
             `;
         });
-        document.getElementById("main").innerHTML=html;
+        document.getElementById("meal-details").innerHTML=html;
+        document.getElementById("meal-details").style.display = 'block';
     }
+
+    // document.querySelector('.btn-close').
+
+        // // Fetch data from rest api
+    async function getData() {
+        for(let i = 0; i < 6; i++) {
+            const response = await fetch("https://www.themealdb.com/api/json/v1/1/random.php")
+            const data = await response.json();
+            dataList = data.meals[0];
+
+            if (data.meals) {
+                html = `
+                <div id="card" class="card mb-3" style="width: 20rem;">
+                <img src="${dataList.strMealThumb}" class="card-img-top" alt="...">
+                <div class="card-body">
+                    <h5 class="card-title">${dataList.strMeal}</h5>
+                    <div class="d-flex justify-content-between mt-5">
+                        <button type="button" class="btn btn-outline-light" onclick="showMealDetails(${dataList.idMeal})">Details</button>
+                        
+                    </div>
+                </div>
+            </div>
+                `
+            ;
+            }else {
+                html += `
+                <div class="page-wrap d-flex flex-row align-items-center">
+                    <div class="container">
+                        <div class="row justify-content-center">
+                            <div class="col-md-12 text-center">
+                                <div class="mb-4 lead" style=">
+                                Le repas que vous recherchez n'a pas été trouvé.
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                `;
+            }
+            document.getElementById("main").innerHTML += html
+        }}
+        getData();
+        var btn_close = document.getElementById('btn-close');
+        function clospopu(){
+            document.getElementById("meal-details").style.display = 'none';
+        };
